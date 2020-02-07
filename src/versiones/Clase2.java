afd1a4de8dd8aabc9351c1c8ee5b3c4f7de983cf
dam <dam@172.26.37.145>
2020-02-07 09:24:17 +0100

    Creación de la clase2.

---
+++ src/versiones/Clase2.java
@@ -0,0 +1,17 @@
+
+package versiones;
+
+/**
+ *
+ * @author Lara
+ */
+public class Clase2 {
+
+    public Clase2() {
+        
+        System.out.println("Creación de la segunda clase");
+        System.out.println("Control de Versiones con GitHub");
+    }
+    
+    
+}
